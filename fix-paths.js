0055d5859ddc4777ac5b8bb3d10e33baa21a7c1a
atsang081 <atsang081@gmail.com>
2025-09-30 09:23:42 +0800

    Fix GitHub Pages deployment configuration

--- fix-paths.js
+++ fix-paths.js
@@ -11,13 +11,13 @@ function fixPathsInFile(filePath) {
   let content = fs.readFileSync(filePath, 'utf8');
   
   // Replace absolute paths with relative paths
-  content = content.replace(/href="\/KidP2\/([^"]*)"/g, 'href="./$1"');
-  content = content.replace(/src="\/KidP2\/([^"]*)"/g, 'src="./$1"');
-  content = content.replace(/href="\/([^"]*)"/g, 'href="./$1"');
-  content = content.replace(/src="\/([^"]*)"/g, 'src="./$1"');
+  content = content.replace(/href="\/KidP2\/([^"]*)"/g, 'href="./KidP2/$1"');
+  content = content.replace(/src="\/KidP2\/([^"]*)"/g, 'src="./KidP2/$1"');
+  content = content.replace(/href="\/([^"]*(?!KidP2)[^"]*)"/g, 'href="./$1"');
+  content = content.replace(/src="\/([^"]*(?!KidP2)[^"]*)"/g, 'src="./$1"');
   
-  // Special case for favicon
-  content = content.replace(/href="\."/g, 'href="./favicon.ico"');
+  // Fix any remaining absolute paths that don't have KidP2
+  content = content.replace(/="\/(?!KidP2)([^"]*)"/g, '="./$1"');
   
   fs.writeFileSync(filePath, content, 'utf8');
   console.log(`Fixed paths in ${filePath}`);
